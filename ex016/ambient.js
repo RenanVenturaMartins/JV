4db391780ef7ec8c7d4d24b22b7f5b27cfc2fd7d
RenanVenturaMartins <40028922Ren@n>
2022-04-22 14:49:33 -0300

    conhecimneto de repetiçoes

---
+++ ex016/ambient.js
@@ -0,0 +1,12 @@
+var area = 8 ; 
+/*while (area > 0) {
+    console.log(` ${area}`) 
+    area-- // area = area - 1
+}*/
+
+
+
+do {
+    console.log (`passo ${area}`)
+    area--
+} while (area > 0)
