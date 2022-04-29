2c4b45cf5236c361797ccfd3bbc3ce4c07e822ae
RenanVenturaMartins <40028922Ren@n>
2022-04-29 16:50:06 -0300

    melhorei o funcionamento do codigo

--- ex019/javascript.js
+++ ex019/javascript.js
@@ -1,21 +1,29 @@
-function calcular(){
-    var cal = document.getElementById('calcular')
-    var one = document.getElementsByName ('formula')
-    var numerico1 = document.getElementById ('n1')
-    var numerico2 = document.getElementById ('n2')
-    var numerico3 = '' 
-    var resultado = document.getElementById ('res')
+function calcular() {
    
-    if (formula[0].checked) {
-         var numerico3 = 'numerico1' + 'numerico2'
-    } else if(formula[1].checked) {
-         var numerico3 =  'numerico1' - 'numerico2'
+   var numerico1 = document.getElementById('n1');
+   var numerico2 = document.getElementById('n2');
+   var operador = document.getElementById('operador');
+   var resultado = document.getElementById('res');
+   var resu = resultado  
 
-    } else if(formula[2].checked) {
-         var numerico3= 'numerico1' * 'numerico2'
-    
-    } else if(formula[3].checked) {
-         var numerico3=  'numerico1' / 'numerico2'
-    }
-    resultado.style.textAlign = 'center'
-    resultado.innerHTML =  `a conta entre ${numerico1} e ${numerico2} deu = ${numerico3} o valor total`}
+   console.log(numerico1.type);
+   console.log(numerico1.value);
+
+  
+
+   if (operador.value == "soma") {
+      resultado = numerico1.value + numerico2.value;
+
+   } else if (operador.value == "subtracao") {
+      resultado = numerico1-numerico2;
+
+   } else if (operador.value == "multiplicacao") {
+      resultado = numerico1*numerico2;
+
+   } else if (operador.value == "divisao") {
+      resultado = numerico1 / numerico2;      
+   };
+
+   resu.innerHTML = `a ${operador.value} entre ${numerico1.value} e ${numerico2.value} = ${resultado}`;
+   
+}
