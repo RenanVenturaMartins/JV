09dd9d021dce9061fba6a6f4601ddc2e0110e9cb
RenanVenturaMartins <40028922Ren@n>
2022-04-29 15:52:30 -0300

    indentei o codigo

--- ex019/javascript.js
+++ ex019/javascript.js
@@ -7,15 +7,15 @@ function calcular(){
     var resultado = document.getElementById ('res')
    
     if (formula[0].checked) {
-       var numerico3 = 'numerico1' + 'numerico2'
+         var numerico3 = 'numerico1' + 'numerico2'
     } else if(formula[1].checked) {
-     var numerico3 =  'numerico1' - 'numerico2'
-    
+         var numerico3 =  'numerico1' - 'numerico2'
+
     } else if(formula[2].checked) {
-     var numerico3= 'numerico1' * 'numerico2'
+         var numerico3= 'numerico1' * 'numerico2'
     
     } else if(formula[3].checked) {
-       var numerico3=  'numerico1' / 'numerico2'
+         var numerico3=  'numerico1' / 'numerico2'
     }
     resultado.style.textAlign = 'center'
     resultado.innerHTML =  `a conta entre ${numerico1} e ${numerico2} deu = ${numerico3} o valor total`}
